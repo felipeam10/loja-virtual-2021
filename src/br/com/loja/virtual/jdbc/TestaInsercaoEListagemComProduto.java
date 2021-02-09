1465ce09ea2ab6781871965cae04b4732639e44e
felipe.araujo <felipe.araujo@sankhya.com.br>
2021-02-09 20:56:13 -0300

    Implementando o DAO

---
+++ src/br/com/loja/virtual/jdbc/TestaInsercaoEListagemComProduto.java
@@ -0,0 +1,24 @@
+package br.com.loja.virtual.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.loja.virtual.jdbc.dao.ProdutoDAO;
+import br.com.loja.virtual.jdbc.modelo.Produto;
+
+public class TestaInsercaoEListagemComProduto {
+
+	public static void main(String[] args) throws SQLException {
+
+		Produto guardaRoupa = new Produto("Guarda Roupas", "06 portas");
+		
+		try (Connection connection = new ConnectionFactory().conectaDatabase()) {
+			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
+			produtoDAO.salvar(guardaRoupa);
+			List<Produto> listaDeProdutos = produtoDAO.listar();
+			listaDeProdutos.stream().forEach(listaProd -> System.out.println(listaProd));
+			
+		}
+	}
+}
